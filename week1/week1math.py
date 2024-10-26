5f43179418ec6c95994d20765a3af69e4c5df79c
Atte Sten <arafsambi@gmail.com>
2024-10-26 18:37:26 +0300

    kotitehtävät

---
+++ week1/week1math.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+a = 2.493
+b = 0.911
+print(np.degrees(a))
+print(np.degrees(b))
+
+c = 137.7
+d = 62.3
+print(f'\n{np.radians(c)} rad')
+print(f'{np.radians(d)} rad\n')
+
+A = np.array([30, 45, 60, 90, 120, 135, 150, 180, 270, 360])
+
+for i in A:
+    print(f"{np.array(i)} deg  I {np.radians(i): .3f} rad")
+
+lävistäjä = 6.4
+suhde_leveys = 3
+suhde_korkeus = 2
+
+kerroin = lävistäjä / np.sqrt(suhde_leveys ** 2 + suhde_korkeus ** 2)
+
+leveys = suhde_leveys * kerroin
+korkeus = suhde_korkeus * kerroin
+
+print(f'\nSuorakulmion sivut ovat {leveys: .1f}m, {korkeus: .1f}m')
+
+
+
+
+
+
+
+
+
+
+
+
+
